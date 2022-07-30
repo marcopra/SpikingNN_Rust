f6810b0106ba17d35f05f2a0613ca1baac0402e9
Davide Miola <davide.miola99@gmail.com>
2022-07-30 10:35:01 +0200

    Removed per-neuron-parallelism option

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,6 @@
 #![cfg_attr(feature = "bench", feature(test))]
 
 mod nn;
-
-#[cfg(feature = "per-neuron-parallelism")]
-mod sync_per_neuron;
-#[cfg(feature = "per-neuron-parallelism")]
-use sync_per_neuron as sync;
-#[cfg(not(feature = "per-neuron-parallelism"))]
 mod sync;
 
 // Re-exports
